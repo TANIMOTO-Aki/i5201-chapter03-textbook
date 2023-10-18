e4c18ef59c761b015d530987adc165effed46bac
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-10-18 11:33:00 +0900

    確認問題3.1 VariableExe.javaを作成しました

---
+++ src/main/java/Variable/VariableExe.java
@@ -0,0 +1,8 @@
+public class VariableExe {
+    public static void main(String[] args) {
+        int num;                            // ①int型変数numの宣言
+
+        num = 100;                          // ②変数numに100を代入
+        System.out.println(num);            // ③変数numの値を表示
+    }
+}
