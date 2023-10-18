1e7548ff44e4585133bf264f4e66119b7b517f29
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-10-18 11:29:06 +0900

    例題プログラムVariable2.javaを作成しました

---
+++ src/main/java/Variable/Variable2.java
@@ -0,0 +1,7 @@
+public class Variable2 {
+    public static void main(String[] args) {
+        int date;                               // ①変数の宣言
+
+        System.out.println(date);               // ②値の参照（表示）
+    }
+}
