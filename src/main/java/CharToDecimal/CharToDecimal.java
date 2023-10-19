1a8a279653f158ad2315506ee855c7a149c6cfb3
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-10-19 09:54:20 +0900

    例題プログラムCharToDecimal.javaを作成しました

---
+++ src/main/java/CharToDecimal/CharToDecimal.java
@@ -0,0 +1,12 @@
+public class CharToDecimal {
+    public static void main(String[] args) {
+        int num;
+        char ch;
+
+        ch = 50;    //値の設定
+        num = ch;   //整数値
+
+        /* 文字の'X'は、10進数 XXXXX で表示する */
+        System.out.println("文字の \'" + ch + "\' は、10進数で " + num);
+    }    
+}
